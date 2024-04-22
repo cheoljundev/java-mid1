6c9e273c335f9814bcb3eebc1b706359eec52dd1
cheoljundev <cheoljundev@gmail.com>
2024-04-22 11:57:29 +0900

    Object 클래스 완료

--- src/lang/object/toString/ToStringMain1.java
+++ src/lang/object/toString/ToStringMain1.java
@@ -2,6 +2,13 @@ package lang.object.toString;
 
 public class ToStringMain1 {
     public static void main(String[] args) {
+        Object object = new Object();
+        String string = object.toString();
 
+        // toString() 반환값 출력
+        System.out.println(string); // java.lang.Object@a09ee92
+
+        // object 직접 출력
+        System.out.println(object); // java.lang.Object@a09ee92
     }
 }
