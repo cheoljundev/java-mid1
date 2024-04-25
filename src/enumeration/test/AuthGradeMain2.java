1a7020e4cde4f25f4d54023f849341112163cfd0
cheoljundev <cheoljundev@gmail.com>
2024-04-25 17:57:17 +0900

    열거형 문제풀이

---
+++ src/enumeration/test/AuthGradeMain2.java
@@ -0,0 +1,13 @@
+package enumeration.test;
+
+import java.util.Scanner;
+
+public class AuthGradeMain2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("당신의 등급을 입력하세요[GUEST, LOGIN, ADMIN]: ");
+        String input = scanner.nextLine();
+        AuthGrade authGrade = AuthGrade.valueOf(input.toUpperCase());
+        authGrade.printMenu();
+    }
+}
