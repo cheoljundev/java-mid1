bd0383e606ac8696520197244d0a03a213a3f2c2
cheoljundev <cheoljundev@gmail.com>
2024-04-29 03:27:09 +0900

    예외 처리

---
+++ src/exception/basic/unchecked/Service.java
@@ -0,0 +1,24 @@
+package exception.basic.unchecked;
+
+public class Service {
+    Client client = new Client();
+
+    /**
+     * 필요한 경우, 예외를 잡아 처리
+     */
+    public void callCatch() {
+        try {
+            client.call();
+        } catch (MyUncheckedException e) {
+            System.out.println("예외 처리, message = " + e.getMessage());
+        }
+        System.out.println("정상 로직");
+    }
+
+    /**
+     * 예외를 잡지 않아도 된다. 자연스럽게 상위로 넘어간다.
+     */
+    public void callThrow() {
+        client.call();
+    }
+}
