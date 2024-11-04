12a9d2745c2e8b6dbda5b54c7844f8e3dd2988df
cheoljundev <cheoljundev@gmail.com>
2024-11-04 23:07:49 +0900

    Object 다형성

---
+++ src/two/lang/Object/poly/ObjectPolyExample1.java
@@ -0,0 +1,21 @@
+package two.lang.Object.poly;
+
+public class ObjectPolyExample1 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Car car = new Car();
+
+        action(dog);
+        action(car);
+    }
+
+    private static void action(Object o) {
+
+        // 객체에 맞는 다운캐스팅 필요
+        if (o instanceof Dog dog) {
+            dog.sound();
+        } else if (o instanceof Car car) {
+            car.move();
+        }
+    }
+}
