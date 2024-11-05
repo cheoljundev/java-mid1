8e5a264c4b6698045061f2c464271afb59d846c6
cheoljundev <cheoljundev@gmail.com>
2024-11-05 15:24:33 +0900

    String 클래스 - 기본

---
+++ src/two/lang/string/CharArrayMain.java
@@ -0,0 +1,14 @@
+package two.lang.string;
+
+public class CharArrayMain {
+    public static void main(String[] args) {
+        char a = '가';
+        System.out.println("a = " + a);
+
+        char[] chars = new char[]{'h', 'e', 'l', 'l', 'o'};
+        System.out.println(chars);
+
+        String str = "hello";
+        System.out.println(str);
+    }
+}
