65ea3df03ba6599dea4c073a9f69284a61700bf1
cheoljundev <cheoljundev@gmail.com>
2024-11-07 02:41:57 +0900

    문자열과 타입 안전성2

---
+++ src/two/lang/enummeration/ex1/StringGradeEx1_2.java
@@ -0,0 +1,27 @@
+package two.lang.enummeration.ex1;
+
+
+public class StringGradeEx1_2 {
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+
+        // 존재하지 않는 등급 입력
+        int vip = discountService.discount("VIP", price);
+
+        // 오타
+        int diaamond = discountService.discount("DIAAMOND", price);
+
+        // 소문자 입력
+        int diamond1 = discountService.discount("diamond", price);
+
+        int basic = discountService.discount(StringGrade.BASIC, price);
+        int gold = discountService.discount(StringGrade.GOLD, price);
+        int diamond = discountService.discount(StringGrade.DIAMOND, price);
+
+        System.out.println("BASIC 등급의 할인금액 : " + basic);
+        System.out.println("GOLD 등급의 할인금액 : " + gold);
+        System.out.println("DIAMOND 등급의 할인금액 : " + diamond);
+    }
+}
